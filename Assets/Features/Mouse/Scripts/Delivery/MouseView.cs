e438191ffc75d4de4d2a733c09a946f42ba68978
IgnGrillo <igngrillo@gmail.com>
2022-12-15 23:57:25 -0300

    Update MouseView, adding a WIP implementation of the ChangeMouseCursor Action

--- Assets/Features/Mouse/Scripts/Delivery/MouseView.cs
+++ Assets/Features/Mouse/Scripts/Delivery/MouseView.cs
@@ -9,9 +9,16 @@ namespace Features.Mouse.Scripts.Delivery
     public class MouseView : MonoBehaviour, IMouseView
     {
         [SerializeField] private LayerMask raycastLayerMask;
+        [SerializeField] private Texture2D defaultMouseSprite;
         
         public event UnitDelegate OnUpdate;
         public LayerMask RaycastLayerMask => raycastLayerMask;
+
+        private void Awake()
+        {
+            //This will be updated by a separate Action, currently here just as an implementation example
+            Cursor.SetCursor(defaultMouseSprite, Vector2.zero, CursorMode.Auto);
+        }
 
         private void Start()
         {

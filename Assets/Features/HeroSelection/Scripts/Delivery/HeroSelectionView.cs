ad8b7382ba4a0968e8de8768fb364917bb6203a6
IgnGrillo <igngrillo@gmail.com>
2022-12-20 16:04:18 -0300

    Update HeroSelectionView, making the class inherit from Mono to be instantiated on the scene

--- Assets/Features/HeroSelection/Scripts/Delivery/HeroSelectionView.cs
+++ Assets/Features/HeroSelection/Scripts/Delivery/HeroSelectionView.cs
@@ -1,10 +1,11 @@
 ﻿using Features.Core.Scripts;
 using Features.HeroSelection.Scripts.Domain;
 using Features.HeroSelection.Scripts.Infrastructure;
+using UnityEngine;
 
 namespace Features.HeroSelection.Scripts.Delivery
 {
-    public class HeroSelectionView : IHeroSelectionView
+    public class HeroSelectionView : MonoBehaviour, IHeroSelectionView
     {
         public event HeroDelegate OnSelection;
         public event UnitDelegate OnConfirmation;
